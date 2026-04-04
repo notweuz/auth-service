c373739660d61e629b91d63b63de4217452d56ed
notweuz <weuz.main@gmail.com>
2026-04-05 02:40:09 +0400

    fix: not handling duplicate keys properly

--- internal/database/user.go
+++ internal/database/user.go
@@ -6,6 +6,7 @@ import (
 	"auth-service/internal/model"
 	"errors"
 
+	"github.com/jackc/pgx/v5/pgconn"
 	"gorm.io/gorm"
 )
 
@@ -17,9 +18,14 @@ func NewUserDatabase(db *gorm.DB) interfaces.UserDatabase {
 	return &userDatabase{db: db}
 }
 
+func isDuplicateKeyError(err error) bool {
+	var pgErr *pgconn.PgError
+	return errors.As(err, &pgErr) && pgErr.Code == "23505"
+}
+
 func (u *userDatabase) Create(user *model.User) (*model.User, error) {
 	if err := u.db.Create(user).Error; err != nil {
-		if errors.Is(err, gorm.ErrDuplicatedKey) {
+		if isDuplicateKeyError(err) {
 			return nil, errs.RecordAlreadyExists
 		}
 		return nil, err
@@ -40,7 +46,7 @@ func (u *userDatabase) FindByID(id uint64) (*model.User, error) {
 
 func (u *userDatabase) Update(user *model.User) (*model.User, error) {
 	if err := u.db.Save(user).Error; err != nil {
-		if errors.Is(err, gorm.ErrDuplicatedKey) {
+		if isDuplicateKeyError(err) {
 			return nil, errs.RecordAlreadyExists
 		}
 		return nil, err
@@ -50,7 +56,7 @@ func (u *userDatabase) Update(user *model.User) (*model.User, error) {
 
 func (u *userDatabase) FindByUsername(username string) (*model.User, error) {
 	var user model.User
-	if err := u.db.First(&user.Username, username).Error; err != nil {
+	if err := u.db.Where("username = ?", username).First(&user).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, errs.RecordNotFound
 		}
